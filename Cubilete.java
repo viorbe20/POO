cb0b2887aa016329ddc06b87a87c7161f2fb52cf
VirginiaOB <72613229+viorbe20@users.noreply.github.com>
2021-02-21 17:09:48 +0100

    Add files via upload

---
+++ Cubilete.java
@@ -0,0 +1,41 @@
+
+public abstract class Cubilete {
+
+
+
+  //Atributos de instancia.
+  private int numeroDados;
+  
+  /**
+   * Método constructor
+   * 
+   */
+  public Cubilete(int dados) {
+    this.numeroDados = dados;
+  }
+
+  /*
+   * Método getter.
+   * @return numeroDados.
+   */
+  public int getNumeroDados() {
+    return numeroDados;
+  }
+
+  /*
+   * Método setter.
+   * @numeroDados
+   */
+  public void setNumeroDados(int numeroDados) {
+    this.numeroDados = numeroDados;
+  }
+  
+  //Otros métodos.
+  
+  /*
+   * Método que tira dado.
+   */
+  {
+    
+  }  
+}
